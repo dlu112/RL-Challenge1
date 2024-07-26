e6b6a901eedb57bbf64beb808c00ec5493ecdf7d
David Lu <davidydlu@gmail.com>
2024-07-26 18:46:06 -0300

    change to file structure and use python logger

--- matrix_finder.py
+++ matrix_finder.py
@@ -1,5 +1,7 @@
 import time
+import logging
 from common import binary_search
+logger = logging.getLogger(__name__)
 
 class MatrixFinder:
     """Search an m x n matrix for a specified target value.
@@ -15,36 +17,24 @@ class MatrixFinder:
         m: An integer representing the number of rows.
         n: An integer representing the number of columns.
         x: The target value.
-        exec_time: Time in ms taken for the search algorithm to complete.
         state: Boolean result of the search algorithm.
-        debug: Boolean whether to print debug messages.
     """
 
-    def __init__(self, matrix, debug=False):
+    def __init__(self, matrix):
         """Initialize MatrixFinder matrix input.
         
         Args:
             matrix (array[float][float]): 
                 Nested list of floats representing a 2d matrix.
-            debug (boolean): Whether to output debug messages, default False.
         """
 
         self._matrix = matrix
-        self.debug = debug
-
         self._m = len(matrix)
         self._n = len(matrix[0])
         self.x = None
-        self._exec_time_ms = 0
         self.state = False
-        
-    def __log_time(self):
-        """Log execution time to console"""
 
-        if self.debug:
-            print("Execution time =", self._exec_time_ms, "ms")
-
-    def __select_row(self):
+    def _select_row(self):
         """Modified binary search to select row target is in.
         
         Returns:
@@ -86,7 +76,7 @@ class MatrixFinder:
         # target is not in the matrix
         return -1
 
-    def __find_x(self, x):
+    def _find_x(self, x):
         """Find row and column of target if in matrix using binary search.
         
         Args:
@@ -96,15 +86,14 @@ class MatrixFinder:
         self.x = x
 
         # search for containing row
-        row = self.__select_row()
+        row = self._select_row()
         if row >= 0:
             # search for containing column
             col = binary_search(self._matrix[row], self.x)
             if col >= 0:
                 self.state = True
-                if self.debug:
-                    print("Target at [{}][{}]".format(row, col))
-    
+                logger.info("Target at [{}][{}]".format(row, col))
+
     def find(self, x):
         """Execute search algorithm and record execution time.
         
@@ -120,20 +109,18 @@ class MatrixFinder:
         
         # catch any TypeErrors
         try:
-            self.__find_x(x)
+            self._find_x(x)
         except TypeError:
-            if self.debug:
-                print("Error: TypeError detected")
+            logger.error("TypeError detected")
             self.state = False
 
-        self._exec_time_ms = (time.process_time_ns() - start_time) // 1000
+        exec_time_ms = (time.process_time_ns() - start_time) // 1000
+        logger.info("Execution time = {} ms".format(exec_time_ms))
         
-        if self.debug:
-            self.__log_time()
-            if self.state:
-                print("SUCCESS!")
-            else:
-                print("FAILURE")
+        if self.state:
+            logger.info("Success!")
+        else:
+            logger.info("Failure!")
 
         return self.state
 
---
+++ test_matrix_finder.py
@@ -0,0 +1,126 @@
+# Unit testing Matrix Finder with unittest
+import unittest
+import logging
+from matrix_finder import MatrixFinder
+
+class TestMatrixFinder(unittest.TestCase):
+    # Initialize test matrices and matrix finders
+    def setUp(self):
+        self.test_matrix = [[0, 3.5, 6, 9],
+                            [10, 13.5, 16, 19],
+                            [20, 23.5, 26, 29],
+                            [30, 33.5, 36, 39],
+                            [40, 43.5, 46, 49],
+                            [50, 53.5, 56, 59]]
+        self.test_matrix_finder = MatrixFinder(self.test_matrix)
+
+        self.test_matrix_2 = [[0, 3],
+                              [10, "apple", 13],
+                              [20, 23]]
+        self.test_matrix_finder_2 = MatrixFinder(self.test_matrix_2)
+
+        self.test_matrix_3 = [[0, 3],
+                              [5, 6],
+                              [7, 8],
+                              ["banana", 10, 13],
+                              [15, 16],
+                              [20, 23]]
+        self.test_matrix_finder_3 = MatrixFinder(self.test_matrix_3)
+
+        self.test_matrix_4 = [[1],
+                              [3],
+                              [5],
+                              [7],
+                              [9]]
+        self.test_matrix_finder_4 = MatrixFinder(self.test_matrix_4)
+
+        self.test_matrix_5 = [[1, 3, 5, 7, 9]]
+        self.test_matrix_finder_5 = MatrixFinder(self.test_matrix_5)
+
+        self.test_matrix_6 = [[5]]
+        self.test_matrix_finder_6 = MatrixFinder(self.test_matrix_6)
+
+    # MatrixFinder gets the correct number of rows
+    def test_rows(self):
+        self.assertEqual(self.test_matrix_finder._m, 6, "Incorrect number of rows")
+
+    # MatrixFinder gets the correct number of columns
+    def test_cols(self):
+        self.assertEqual(self.test_matrix_finder._n, 4, "Incorrect number of columns")
+
+    # Each row is the correct length
+    def test_row_lengths(self):
+        for row in self.test_matrix_finder._matrix:
+            self.assertEqual(len(row), 4, "A row in the matrix has the wrong length")
+
+    # Finds target correctly
+    def test_find_target(self):
+        self.assertTrue(self.test_matrix_finder.find(36), "Fails to find target in matrix")
+
+    # Fails to find target correctly
+    def test_fails_target(self):
+        self.assertFalse(self.test_matrix_finder.find(37), "Finds target not in matrix")
+    
+    # Finds target when target is in matrix[0][0]
+    def test_find_first_target(self):
+        self.assertTrue(self.test_matrix_finder.find(0), "Fails to find target in matrix located at [0][0]")
+
+    # Finds target when target is in matrix[m][n]
+    def test_find_last_target(self):
+        self.assertTrue(self.test_matrix_finder.find(59), "Fails to find target in matrix located at [m][n]")
+
+    # Fails when target is less than matrix[0][0]
+    def test_target_too_small(self):
+        self.assertFalse(self.test_matrix_finder.find(-1), "Finds target smaller than smallest value in matrix")
+
+    # Fails when target is greater than highest value in matrix
+    def test_target_too_big(self):
+        self.assertFalse(self.test_matrix_finder.find(60), "Finds target larger than highest value in matrix")
+
+    # Fails when target is wrong type
+    def test_target_type_error(self):
+        self.assertFalse(self.test_matrix_finder.find("10"), "Fails to handle TypeError in target")
+
+    # Succeeds when TypeError is in matrix but not interacted with
+    def test_avoid_matrix_type_error(self):
+        self.assertTrue(self.test_matrix_finder_2.find(3), "Fails to avoid TypeError in matrix row")
+    
+    # Fails when TypeError is in selected target row
+    def test_handle_matrix_type_error(self):
+        self.assertFalse(self.test_matrix_finder_2.find(13), "Fails to handle TypeError in matrix")
+
+    # Succeeds when TypeError is in first column of matrix and not interacted with
+    def test_avoid_matrix_type_error_col_1(self):
+        self.assertTrue(self.test_matrix_finder_3.find(0), "Fails to avoid TypeError in first column of matrix")
+
+    # Fails when TypeError is in first column of matrix and interacted with
+    def test_handle_matrix_type_error_col_1(self):
+        self.assertFalse(self.test_matrix_finder_3.find(15), "Fails to handle TypeError in first column of matrix")
+
+    # Successfully finds value when matrix has n = 1
+    def test_find_target_n_1(self):
+        self.assertTrue(self.test_matrix_finder_4.find(3), "Fails to find target in one column matrix")
+
+    # Successfully fails to finds value when matrix has n = 1
+    def test_fails_target_n_1(self):
+        self.assertFalse(self.test_matrix_finder_4.find(2), "Find target not in in one column matrix")
+
+    # Successfully finds value when matrix has m = 1
+    def test_find_target_n_1(self):
+        self.assertTrue(self.test_matrix_finder_5.find(3), "Fails to find target in one row matrix")
+
+    # Successfully fails to finds value when matrix has m = 1
+    def test_fails_target_n_1(self):
+        self.assertFalse(self.test_matrix_finder_5.find(2), "Find target not in in one row matrix")
+
+    # Successfully finds value when matrix s of dimension 1 x 1
+    def test_find_target_1x1(self):
+        self.assertTrue(self.test_matrix_finder_6.find(5), "Fails to find target in 1x1 matrix")
+
+    # Successfully fails to finds value when matrix is of dimension 1 x 1
+    def test_fails_target_1x1(self):
+        self.assertFalse(self.test_matrix_finder_6.find(4), "Find target not in in 1x1 matrix")
+    
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    unittest.main()
